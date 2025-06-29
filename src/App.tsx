aaad4d83717f7be758bdde5a7576bf30038b0560
dwl21 <nggus5@gmail.com>
2025-06-29 15:04:50 +0900

    feat: 예제 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { TopNavBar } from './components/TopNavBar'
 import { Home } from './pages/Home'
-import { About } from './pages/About'
-import { Contact } from './pages/Contact'
-
 import { NotFound } from './pages/NotFound'
 import './App.css'
 
@@ -21,8 +18,6 @@ function App() {
         }}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
